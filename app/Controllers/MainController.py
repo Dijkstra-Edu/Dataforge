e9c3a401c5a6ef900ca34e3b528f5f2642f8bf95
SJ-Cipher <sajulin.johanan@gmail.com>
2025-07-18 14:31:59 +0530

    Updated Code with Sajulin Backend

--- app/Controllers/MainController.py
+++ app/Controllers/MainController.py
@@ -5,10 +5,10 @@ from Services import GitHubService
 # GET Commands
 async def getGitHubData(userName: str, params: SearchParams):
     if params:
-        return GitHubService.getGitHubDataWithSearchParams(userName, params)
+        return await GitHubService.getGitHubDataWithSearchParams(userName, params)
     else:
-        return GitHubService.getAllGitHubData(userName)
+        return await GitHubService.getAllGitHubData(userName)
 
 # POST Commands
-async def postDownloadCertificate(userName: str):
+async def postDownloadCertificate (userName: str):
     return CertificateGeneratorService.mainCertificateGeneratorService(userName)
